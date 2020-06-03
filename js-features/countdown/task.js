754b5ba8c09d4c91ed16127b9711290900121b2d
fjfoxru <61179622+fjfoxru@users.noreply.github.com>
2020-06-03 21:45:57 +0300

    Update task.js

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,18 +1,18 @@
-const nowSeconds = document.getElementById('timer');
-const timeNow = new Date;
-
-
+const nowSecondsContainer = document.getElementById('timer');
+let nowSeconds = ('00:' + nowSecondsContainer.textContent).split(':');
 let interval;
 
 
 const timer = () => {
-    if (nowSeconds.textContent == '0') {
-        alert('Вы победили в конкурсе!');
-        clearInterval(interval);
-        return;
-    }
-        nowSeconds.textContent -= 1;
     
+        nowSeconds[1] -= 1;
+        nowSecondsContainer.textContent = nowSeconds.join(':');
+        if (nowSecondsContainer.textContent == '00:0') {
+            alert('Вы победили в конкурсе!');
+            clearInterval(interval);
+            return;
+        }
+        
 }
 (() => interval = setInterval(timer, 1000))();
 
