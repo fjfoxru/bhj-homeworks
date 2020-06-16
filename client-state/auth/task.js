0a3c89561874de4f13d9a058c5d919c10ca52662
Alexandra Lyashenko <fjfox@yandex.ru>
2020-06-16 19:13:45 +0300

    fixed

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -1,24 +1,30 @@
 const loginForm = document.getElementById('signin__form');
-loginForm.closest('.signin').classList.add('signin_active');
+const welcome = document.getElementById('welcome');
+const userId = document.getElementById('user_id');
 
-
-
-loginForm.addEventListener('submit', (event) => {
-    let formData = new FormData(loginForm);
-    let xhr = new XMLHttpRequest();
-    xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/auth.php');
-    xhr.addEventListener('readystatechange', () => {
-        if (xhr.readyState === xhr.DONE) {
-            if (xhr.responseText.success) {
-                const userId = document.getElementById('user_id');
-                welcom.closest('.welcome').listClass.add('welcome_active');
-                userId.innerText(`${xhr.responseText.id}`);
-                localStorage.setItem('userId', 'xhr.responseText.id');
-            } else {
-                alert('Неверный логин/пароль');
+if (localStorage.getItem('userId') !== null) {
+    welcome.closest('.welcome').classList.add('welcome_active');
+    userId.textContent = localStorage.userId;
+} else {
+    loginForm.closest('.signin').classList.add('signin_active');
+    loginForm.addEventListener('submit', (event) => {
+        let formData = new FormData(loginForm);
+        let xhr = new XMLHttpRequest();
+        xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/auth.php');
+        xhr.addEventListener('readystatechange', () => {
+            if (xhr.readyState === xhr.DONE) {
+                const response = JSON.parse(xhr.responseText);
+                if (response.success) {
+                    userId.textContent = response.user_id;
+                    welcome.closest('.welcome').classList.add('welcome_active');
+                    localStorage.setItem('userId', response.user_id);
+                    loginForm.closest('.signin').classList.remove('signin_active');
+                } else {
+                    alert('Неверный логин/пароль');
+                }
             }
-        }
+        });
+        xhr.send(formData);
+        event.preventDefault();
     });
-    xhr.send(formData);
-    event.preventDefault();
-});
+}
