6a119f983dc17ca925bb69fb405aa52f1e4ad14a
Alexandra Lyashenko <fjfox@yandex.ru>
2020-06-17 09:40:32 +0300

    fix

--- client-state/text-editor/task.js
+++ client-state/text-editor/task.js
@@ -1,5 +1,9 @@
 const textArea = document.getElementById('editor');
 
+if (localStorage.text != null) {
+    textArea.value = localStorage.text;
+}
+
 textArea.addEventListener('keyup', () => {
     localStorage.text = textArea.value;
 });
